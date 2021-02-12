02d4eb0bd872ce625da14a7ac5f7bb527a0d0bcb
Quentin Chen <qunsheng.chen@hotmail.com>
2021-02-12 18:54:39 -0500

    recursive component

---
+++ step019_recursive_component/README.md
@@ -0,0 +1,1 @@
+# Step 019: Recursive Component
---
+++ step019_recursive_component/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import FamilyTree from './components/familyTree';
+import members from './family';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>My Family Tree</h3>
+        <FamilyTree members={members}  />
+      </div>
+    );
+  }
+}
+
+export default App;
