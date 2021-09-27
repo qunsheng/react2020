a5a63635d190ea6a254a90469f274f8f139e4b30
Quentin Chen <qunsheng_chen@hotmail.com>
2021-09-27 17:17:19 -0400

    react grid layout

---
+++ step028_react_grid_layout/README.md
@@ -0,0 +1,12 @@
+# Step 028: create user list app
+
+copy code from step022
+
+Run `npm install`
+
+Run `npm install react-grid-layout`
+Run `npm install lodash`
+
+Run `npm start`
+
+
---
+++ step028_react_grid_layout/src/App.js
@@ -0,0 +1,259 @@
+import React from 'react';
+// import GridLayout from 'react-grid-layout';
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
+import './example-styles.css';
+import _ from "lodash";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+class ToolBoxItem extends React.Component {
+  render() {
+    return (
+      <div
+        className="toolbox__items__item"
+        onClick={this.props.onTakeItem.bind(undefined, this.props.item)}
+      >
+        {this.props.item.i}
+      </div>
+    );
+  }
+}
+class ToolBox extends React.Component {
+  render() {
+    return (
+      <div className="toolbox">
+        <span className="toolbox__title">Toolbox</span>
+        <div className="toolbox__items">
+          {this.props.items.map(item => (
+            <ToolBoxItem
+              key={item.i}
+              item={item}
+              onTakeItem={this.props.onTakeItem}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class ToolboxLayout extends React.Component {
+  static defaultProps = {
+    className: "layout",
+    rowHeight: 12,
+    onLayoutChange: function() {},
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+    initialLayout: generateLayout()
+  };
+
+  state = {
+    currentBreakpoint: "lg",
+    // compactType: "vertical",
+    compactType: "horizontal",
+    mounted: false,
+    layouts: { lg: this.props.initialLayout },
+    toolbox: { lg: [] }
+  };
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts[this.state.currentBreakpoint], l => {
+      return (
+        <div key={l.i} className={l.static ? "static" : ""}>
+          <div className="hide-button" onClick={this.onPutItem.bind(this, l)}>
+            &times;
+          </div>
+
+
+          <div style={{
+                    color: "green",
+                    height: "90%",
+                    width: "90%",
+                    background: "red",
+                    overflow: "auto"
+                  }}
+          >
+
+          {l.static ? (
+            <span
+              className="text"
+              title="This item is static and cannot be removed or resized."
+            >
+              Static - {l.i}
+            </span>
+          ) : (
+            <span className="text">{l.i}</span>
+          )}
+          xxxxxxxxxxxxxxxx11111111111111111133333333333333333333333333333333333333333333333333333333
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            55555555555555555555
+            <br></br>
+          </div>
+
+        </div>
+      );
+    });
+  }
+
+  onBreakpointChange = breakpoint => {
+    console.log("================= on break point change");
+    this.setState(prevState => ({
+      currentBreakpoint: breakpoint,
+      toolbox: {
+        ...prevState.toolbox,
+        [breakpoint]:
+          prevState.toolbox[breakpoint] ||
+          prevState.toolbox[prevState.currentBreakpoint] ||
+          []
+      }
+    }));
+  };
+
+  onCompactTypeChange = () => {
+    console.log("================= onCompactTypeChange");
+    const { compactType: oldCompactType } = this.state;
+    const compactType =
+      oldCompactType === "horizontal"
+        ? "vertical"
+        : oldCompactType === "vertical"
+        ? null
+        : "horizontal";
+    this.setState({ compactType });
+  };
+
+  onTakeItem = item => {
+    this.setState(prevState => ({
+      toolbox: {
+        ...prevState.toolbox,
+        [prevState.currentBreakpoint]: prevState.toolbox[
+          prevState.currentBreakpoint
+        ].filter(({ i }) => i !== item.i)
+      },
+      layouts: {
+        ...prevState.layouts,
+        [prevState.currentBreakpoint]: [
+          ...prevState.layouts[prevState.currentBreakpoint],
+          item
+        ]
+      }
+    }));
+  };
+
+  onPutItem = item => {
+    this.setState(prevState => {
+      return {
+        toolbox: {
+          ...prevState.toolbox,
+          [prevState.currentBreakpoint]: [
+            ...(prevState.toolbox[prevState.currentBreakpoint] || []),
+            item
+          ]
+        },
+        layouts: {
+          ...prevState.layouts,
+          [prevState.currentBreakpoint]: prevState.layouts[
+            prevState.currentBreakpoint
+          ].filter(({ i }) => i !== item.i)
+        }
+      };
+    });
+  };
+
+  onLayoutChange = (layout, layouts) => {
+    console.log("=================  onLayoutChange");
+    this.props.onLayoutChange(layout, layouts);
+    this.setState({ layouts });
+  };
+
+  onNewLayout = () => {
+    this.setState({
+      layouts: { lg: generateLayout() }
+    });
+  };
+
+  render() {
+    console.log("============= render state: ", this.state );
+    console.log("============= this.props: ", this.props );
+    return (
+      <div>
+        <div>
+          Current Breakpoint: {this.state.currentBreakpoint} (
+          {this.props.cols[this.state.currentBreakpoint]} columns)
+        </div>
+        <div>
+          Compaction type:{" "}
+          {_.capitalize(this.state.compactType) || "No Compaction"}
+        </div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <button onClick={this.onCompactTypeChange}>
+          Change Compaction Type
+        </button>
+
+        <ToolBox
+          items={this.state.toolbox[this.state.currentBreakpoint] || []}
+          onTakeItem={this.onTakeItem}
+        />
+
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
+
+// function generateLayout() {
+//   return _.map(_.range(0, 10), function(item, i) {
+//     var y = Math.ceil(Math.random() * 4) + 1;
+//     return {
+//       x: (_.random(0, 5) * 2) % 12,
+//       // x: 60,
+//       y: Math.floor(i / 6) * y,
+//       // y: 0,
+//       w: 12,
+//       h: 6,
+//       i: i.toString(),
+//       static: Math.random() < 0.05
+//     };
+//   });
+// }
+
+function generateLayout() {
+  return _.map(_.range(0, 10), function(item, i) {
+    var y = Math.ceil(Math.random() * 8) + 1;
+    return {
+      x: (_.random(0, 5) * 2) % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: Math.random() < 0.05
+    };
+  });
+}
+
+// if (process.env.STATIC_EXAMPLES === true) {
+//   import("../test-hook.jsx").then(fn => fn.default(ToolboxLayout));
+// }
